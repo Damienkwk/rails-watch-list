90df3833ea8618a89282479a82c94c9100ee1507
Damien Koh <damienkwk@hotmail.sg>
2023-02-19 14:19:16 +0800

    create watch list app basic features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'movies/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new", as: :new_lists
+  # post "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show", as: :list
+  resources :lists, only: [:create, :index, :new, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+serialized = URI.open(url).read
+list = JSON.parse(serialized)
+
+list["results"].each do |result|
+  Movie.create!(
+    title: result["original_title"],
+    overview: result["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w200#{result['poster_path']}",
+    rating: result["vote_average"]
+  )
+end
